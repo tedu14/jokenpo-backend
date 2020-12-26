05e90ff26d5c205926016928f63aaf01391a3084
tedu14 <thalison.eduardo14@gmail.com>
2020-12-25 23:40:51 -0400

    feat: create move controller method

--- src/modules/game/infra/controllers/MoveController.ts
+++ src/modules/game/infra/controllers/MoveController.ts
@@ -1,4 +1,6 @@
+import IdGenerate from '@game/providers/idGenerate/implementation/uuidv4'
 import MoveFakeRepority from '@game/repositories/fake/MoveFakeRepository'
+import CreateMoveService from '@game/services/CreateMoveService'
 import FindMoveService from '@game/services/FindMoveService'
 import MissingParamError from '../errors/MissingParamError'
 import { badRequest, serverError, successRequest } from '../helpers/httpHelper'
@@ -19,6 +21,36 @@ export default class MoveController {
             const playerMove = await findMoveService.execute(player_id)
 
             return successRequest(playerMove)
+        } catch (err) {
+            return serverError()
+        }
+    }
+
+    public async create(request: httpRequest): Promise<httpResponse> {
+        try {
+            const { move, player_id } = request.body
+
+            const moveFakeRepository = new MoveFakeRepority()
+            const idGenerate = new IdGenerate()
+            const createMoveService = new CreateMoveService(
+                moveFakeRepository,
+                idGenerate
+            )
+
+            if (!move) {
+                return badRequest(new MissingParamError('move'))
+            }
+
+            if (!player_id) {
+                return badRequest(new MissingParamError('Player id'))
+            }
+
+            const createdMove = await createMoveService.execute({
+                move,
+                player_id
+            })
+
+            return successRequest(createdMove)
         } catch (err) {
             return serverError()
         }
