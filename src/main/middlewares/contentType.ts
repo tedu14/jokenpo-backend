db43b6bfb79d4aecc0f7c0741e92668b9a6cdf40
tedu14 <thalison.eduardo14@gmail.com>
2020-12-26 16:49:49 -0400

    feat: create a middlewares

---
+++ src/main/middlewares/contentType.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from 'express'
+
+export default (req: Request, res: Response, next: NextFunction): void => {
+    res.type('json')
+
+    next()
+}
---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+
+export default (req: Request, res: Response, next: NextFunction): void => {
+    res.set('access-control-allow-origin', '*')
+    res.set('access-control-allow-methods', '*')
+    res.set('access-control-allow-headers', '*')
+
+    next()
+}
